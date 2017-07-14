cac8fe9ba7aa70426e9528db29f31cd4e4b044c4
raksha78 <rakshitha27@gmail.com>
2017-07-14 11:49:14 +0530

    updating scala versions

---
+++ version/2_11_11.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+echo "================= Install Scala 2.11.11 ==================="
+wget -nv http://downloads.lightbend.com/scala/2.11.11/scala-2.11.11.tgz
+tar xzf scala-2.11.11.tgz -C /usr/local && rm -f scala-2.11.11.tgz
