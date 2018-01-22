591338c6fa0305a44a01b8d3a241ea22e4cbc2da
Niranjan Godbole <niranjan8192@gmail.com>
2018-01-22 09:27:53 +0000

    update u16 scala versions

--- version/2_11_11.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-echo "================= Install Scala 2.11.11 ==================="
-wget -nv http://downloads.lightbend.com/scala/2.11.11/scala-2.11.11.tgz
-tar xzf scala-2.11.11.tgz -C /usr/local && rm -f scala-2.11.11.tgz
--- version/2_9_3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-echo "================= Install Scala 2.9.3 ==================="
-wget -nv http://www.scala-lang.org/files/archive/scala-2.9.3.tgz
-tar xzf scala-2.9.3.tgz -C /usr/local && rm -f scala-2.9.3.tgz
