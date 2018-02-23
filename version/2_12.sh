3ab7976a6710b3012d6506f0ac8cc8d00054b881
Ankul Kumar <ankul@shippable.com>
2018-02-23 15:11:10 +0530

    export envs in /etc/drydock/.env

--- version/2_12.sh
+++ version/2_12.sh
@@ -5,6 +5,5 @@ echo "================= Install Scala $SCALA_VER ==================="
 wget -nv http://downloads.lightbend.com/scala/$SCALA_VER/scala-"$SCALA_VER".tgz
 tar xzf scala-"$SCALA_VER".tgz -C /usr/local && rm -f scala-"$SCALA_VER".tgz
 #export paths only for the lastest version
-echo "export SCALA_HOME=/usr/local/scala-$SCALA_VER" >> $HOME/.bashrc
-echo 'export PATH=$PATH:$SCALA_HOME/bin' >> $HOME/.bashrc
-
+echo "export SCALA_HOME=/usr/local/scala-$SCALA_VER" >> /etc/drydock/.env
+echo 'export PATH=$PATH:$SCALA_HOME/bin' >> /etc/drydock/.env
