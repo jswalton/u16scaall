a2f16d4af0dd64b3858bd27c3ea014426872c850
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-04-26 11:15:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt-get update
 echo "================= Install sbt ==================="
 wget -nv https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.13/sbt-launch.jar
 mv sbt-launch.jar /usr/local/bin/sbt-launch.jar
-echo 'SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"' > /usr/local/bin/sbt
+echo 'SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled"' > /usr/local/bin/sbt
 echo 'java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"' >> /usr/local/bin/sbt
 chmod +x /usr/local/bin/sbt
 printf 'exit\n' | sbt
